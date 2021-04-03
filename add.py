7a37825b903d88046ddd837016a445afff1bfc15
saipranav <saipranav786@gmail.com>
2021-04-03 16:14:17 +0530

    Add every two elements in a list

--- add.py
+++ add.py
@@ -3,6 +3,6 @@ def add(input1,input2):
     for i in range(input1-1):
         addi=input2[i] + input2[i+1]
         sum=total+addi
+        print(sum)
     return sum
-add(2,[1,2])
-
+add(5,[10,20,30,40,50])

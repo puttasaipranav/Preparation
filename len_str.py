520c2aa279568a866949780142bbef17c562c404
saipranav <saipranav786@gmail.com>
2021-04-04 20:41:28 +0530

    length

--- len_str.py
+++ len_str.py
@@ -1,3 +1,5 @@
+#Write a Python program to count the number of strings 
+# where the string length is 2 or more
 def leng(a):
     g=[]
     for i in a:
@@ -5,3 +7,5 @@ def leng(a):
             g.append(i)
     print(len(g))
 leng(['sai','f','fg'])
+
+#Output : 2

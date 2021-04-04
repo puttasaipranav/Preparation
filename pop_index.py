70ab62a7ae3dc668732604c0b752c0b53da73f6d
saipranav <saipranav786@gmail.com>
2021-04-04 20:41:52 +0530

    done

--- pop_index.py
+++ pop_index.py
@@ -1,6 +1,9 @@
+#Write a Python program to print a specified list 
+# after removing the 0th, 4th and 5th elements
 def rem(a):
     a.pop(0)
     a.pop(3)
     a.pop(4)
     print(a)
 rem([1,2,3,4,5,6,7])
+# Output: [2, 3, 4, 6]

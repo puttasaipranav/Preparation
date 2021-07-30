bafdbb9ed5b3a16e91d3f684a5186ce6acd8de66
saipranav <saipranav786@gmail.com>
2021-07-30 11:09:35 +0530

    CTR data collection and visualisation

---
+++ CTR.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+df = pd.read_csv("d.csv",header=2)
+n=1
+df.drop(df.tail(n).index, inplace = True)
+
+f=df["Device"]=="Computers"
+g=df.loc[f]
+conv=g["Impr."]
+conv = conv.str.replace(',','').astype(int)
+con=conv.sum()
+cos=g["Clicks"]
+cos = cos.str.replace(',','').astype(int)
+cos= cos.sum()
+ctr = cos/con*100
+
+a= df['Device']=='Mobile phones'
+b= df.loc[a]
+mconv= b['Impr.'].str.replace(',','').astype(int)
+mconv=mconv.sum()
+mcos=b['Clicks'].str.replace(',','').astype(int)
+mcos=mcos.sum()
+mctr=mcos/mconv*100
+
+c= df['Device']=='Tablets'
+d= df.loc[c]
+nconv= d['Impr.'].str.replace(',','').astype(int)
+nconv= nconv.sum()
+ncos= d['Clicks'].str.replace(',','').astype(int)
+ncos= ncos.sum()
+nctr= ncos/nconv*100
+
+print("Clicks")
+print("C Clicks:",cos)
+print("M Clicks:",mcos)
+print("T Clicks:",ncos)
+
+print("CTR's")
+print("C CPC: $",ctr)
+print("M CPC: $",mctr)
+print("T CPC: $",nctr)
+
+lis=[]
+lis.append(ctr)
+lis.append(mctr)
+lis.append(nctr)
+
+
+mylabels=['computers','Mobile','Tablet']
+plt.pie(lis,labels=mylabels)
